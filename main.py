84fe12e4e79fad67c4ba62285e13cf6e59e56a73
Albert WSL <dewatrigant.albert@gmail.com>
2024-01-21 12:17:06 +0100

    poetry added to project

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from src.conf import Config
-import requests
 from pprint import pprint
 from src import strava
 
 strava_client = strava.Client()
-# pprint(len(strava_client.get_athlete_activities(per_page=100, before = "01/12/2023")))
-pprint(strava_client.get_athlete_activities()[0])
+pprint(strava_client.get_athlete_activities(per_page=100, output_format="DF"))
+# pprint(strava_client.get_athlete_activities()[2])
+# pprint(strava_client.get_activity_data(10556240113))
+# pprint(strava_client.get_athlete_data(info = "zones"))
+# pprint(strava_client.get_equipment("g14010307"))
--- src/strava.py
+++ src/strava.py
@@ -1,16 +1,21 @@
-import requests
-from src.conf import Config
-from dotenv import set_key
-from pathlib import Path
 import time
 from datetime import datetime
+from pathlib import Path
 from typing import List
 
+import pandas as pd
+import requests
+from dotenv import set_key
+
+from src.conf import Config
+
 ENV_FILE_PATH = Path("./.env")
+
 
 class Client:
     """Client to interact with Strava API
     """
+
     def __init__(self) -> None:
         self.client_id = Config.CLIENT_ID
         self.client_secret = Config.CLIENT_SECRET
@@ -28,52 +33,108 @@ class Client:
             endpoint = "https://www.strava.com/api/v3/athlete"
             response = self._run_request(endpoint)
             athlete_id = response["id"]
-            set_key(dotenv_path=ENV_FILE_PATH, key_to_set="ATHLETE_ID", value_to_set=str(athlete_id))
+            set_key(dotenv_path=ENV_FILE_PATH, key_to_set="ATHLETE_ID",
+                    value_to_set=str(athlete_id))
         return athlete_id
 
-    def get_athlete_stats(self) -> dict:
+    def get_athlete_data(self, info: str = None) -> dict:
         """Function that gets all general athlete stats
 
+        Args:
+            info (str, optional): Can be "routes", "zones" or "stats". 
+            If None, the athlete itself is returned. Defaults to None.
+
         Returns:
-            dict: athlete stats
+            dict: athlete data
         """
-        athlete_id = self.get_athlete_id()
-        endpoint = f"https://www.strava.com/api/v3/athletes/{athlete_id}/stats"
+        if info:
+            if info.lower() == "zones":
+                return self._get_athlete_zones()
+            athlete_id = self.get_athlete_id()
+            info = "/" + info.lower()
+            endpoint = f"https://www.strava.com/api/v3/athletes/{athlete_id}{info}"
+        else:
+            endpoint = "https://www.strava.com/api/v3/athlete"
         response = self._run_request(endpoint)
         return response
-    
-    def get_athlete_activities(self, after: str = None, before: str = None, per_page: int = None) -> dict:
+
+    def _get_athlete_zones(self) -> List:
+        """Get heart zones of athlete
+
+        Returns:
+            List: List of different heart zones
+        """
+        endpoint = "https://www.strava.com/api/v3/athlete/zones"
+        response = self._run_request(endpoint)
+        return response["heart_rate"]["zones"]
+
+    def get_athlete_activities(
+        self, after: str = None, 
+        before: str = None, 
+        per_page: int = None,
+        output_format: str = "JSON" 
+        ) -> dict:
         """Function that gets all activities of an athlete
 
         Args:
             after (str, optional): Start date of the list (DD/MM/YYYY). Defaults to None.
             before (str, optional): End date of the list (DD/MM/YYYY). Defaults to None.
             per_page (int, optional): number of elements to retrieve. Defaults to None.
+            output_format (str, optional): output desired format. Defaults to JSON.
 
         Returns:
             dict: all activities in dict format
         """
-        params = locals()
-        del params["self"]
+        params = {}
+        if per_page:
+            params["per_page"] = per_page
+        
         if after:
-            params["after"] = int(time.mktime(datetime.strptime(after, "%d/%m/%Y").timetuple()))
+            params["after"] = int(time.mktime(
+                datetime.strptime(after, "%d/%m/%Y").timetuple()))
+        
         if before:
-            params["before"] = int(time.mktime(datetime.strptime(before, "%d/%m/%Y").timetuple()))
+            params["before"] = int(time.mktime(
+                datetime.strptime(before, "%d/%m/%Y").timetuple()))
+        
         endpoint = f"https://www.strava.com/api/v3/athlete/activities"
-            
+
         response = self._run_request(endpoint, params)
+        
+        if output_format.upper() == "DF":
+            response = pd.json_normalize(response)
         return response
 
-    def get_athlete_zones(self) -> List:
-        """Get heart zones of athlete
+    def get_activity_data(self, activity_id: int, info: str = None) -> List:
+        """Get infos about an activity
+
+        Args:
+            activity_id (int): ID of the activity to analyze
+            info (str, optional): Can be "comments", "laps" or "kudos". 
+            If None, the activity itself is returned. Defaults to None.
 
         Returns:
-            List: List of different heart zones
+            List: Infos to return
         """
-        endpoint = f"https://www.strava.com/api/v3/athlete/zones"
+        if info:
+            info = "/" + info.lower()
+        endpoint = f"https://www.strava.com/api/v3/activities/{activity_id}{info}"
         response = self._run_request(endpoint)
-        return response["heart_rate"]["zones"]
-    
+        return response
+
+    def get_equipment(self, gear_id: str) -> dict:
+        """Get info about an equipment
+
+        Args:
+            gear_id (str): ID of the equipment to analyze
+
+        Returns:
+            dict: equipment infos
+        """
+        endpoint = f"https://www.strava.com/api/v3/gear/{gear_id}"
+        response = self._run_request(endpoint)
+        return response
+
     def _get_access_token(self) -> str:
         """Function to get current access token, based on refresh token
 
@@ -81,17 +142,17 @@ class Client:
             str: access token in string format
         """
         params = {
-            "client_id" : self.client_id,
-            "client_secret" : self.client_secret,
-            "refresh_token" : self.refresh_token,
-            "grant_type" : "refresh_token"
+            "client_id": self.client_id,
+            "client_secret": self.client_secret,
+            "refresh_token": self.refresh_token,
+            "grant_type": "refresh_token"
         }
         url = "https://www.strava.com/api/v3/oauth/token"
-        
+
         response = requests.post(url, params=params)
         access_token = response.json()["access_token"]
         return access_token
-            
+
     def _run_request(self, endpoint: str, params: dict = None) -> dict:
         """Function to run a get request on an endpoint
 
@@ -104,6 +165,6 @@ class Client:
         """
         token = self._get_access_token()
         headers = {"Authorization": f"Bearer {token}"}
-        response = requests.get(endpoint, headers=headers, params=params).json()
+        response = requests.get(
+            endpoint, headers=headers, params=params).json()
         return response
-        
